40fb160bac7487467e8f240f6a0ee1a59e4b1103
REGZ-PL <96737586+REGZ-PL@users.noreply.github.com>
2022-04-07 10:40:57 +0000

    Update IP2Country.php

--- IP2Country.php
+++ IP2Country.php
@@ -1,1 +1,17 @@
-<?php
+<?PHP
+
+	FUNCTION IP2COUNTRY($IP) {
+		$OUTPUT = ''; $MATCHES = [];
+		IF ($FP = @FSOCKOPEN("whois.ripe.net", 43, $ERRNO, $ERRSTR, 30)) {
+			IF(FWRITE($FP, $IP . "\r\n")) {
+				WHILE (!FEOF($FP)) {
+					$OUTPUT .= FGETS($FP, 128);
+        }
+				FCLOSE($FP);
+				IF (PREG_MATCH("/country:\s+([a-z]{2})/i", $OUTPUT, $MATCHES)) {
+					RETURN $MATCHES[1];
+        }
+			}
+		}
+		RETURN FALSE;
+	}
